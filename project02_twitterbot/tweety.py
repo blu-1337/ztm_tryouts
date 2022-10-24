31c2e54d5c9d4ae80a3159f3a137b341a96ddd4a
blu <n1ilmpg@gmail.com>
2022-10-24 17:11:57 +0300

    finishes up second project

--- project02_twitterbot/tweety.py
+++ project02_twitterbot/tweety.py
@@ -2,9 +2,9 @@ import tweepy
 import time
 
 auth = tweepy.OAuth1UserHandler(
-    'O7BgAl4ouoIOcncOj5n4bfr1F', 'iAScv87b1LVSQy1iln2UE9L3sJztUTscmlryuDLy8GlF9zDOpA',
-    '188697633-BROurss1QQFNcKo8sANipnvTnSCSc9xIGwelSpwG', 'x0XwJcdBUMbom7G5Swp1SCaqNQX4QKR5e9kRMixHamBFx'
-)
+    'wx02sMuDQkqUnFBvgRpi4gP80', 'VSs3j2FLOhPfaAKG06CvcLnQJq6T6xirzv8mtdIPGPtH1aoeTB',
+    '188697633-irPiBpmnGWYMNQ2fCHkhsgKpdKJ0ZFFr96vL67Vr', '4ZgCnBrcYW8UQig4oDJYhxOu12yPj4e3wcCuWJRIkLJwe'
+)  # you get this from your twitter api development profile, cgwiki too
 
 api = tweepy.API(auth)
 
@@ -12,8 +12,7 @@ public_tweets = api.home_timeline()
 for tweet in public_tweets:
     print(tweet.text)  # gets all our tweets
 
-user = api.get_user
-print(user)
+
 
 
 def limit_handler(cursor):  # do not fetch everything at once, avoid flooding
@@ -26,22 +25,22 @@ def limit_handler(cursor):  # do not fetch everything at once, avoid flooding
         return
 
 
-# # Generous Bot
+# Generous Bot: this follows back a certain follower if their name is on your followers list
 # for follower in limit_handler(tweepy.Cursor(api.get_followers).items()):
-#     if follower.name == "ElonMusk":
+#     if follower.name == "Elon Musk":
 #         follower.follow()
 #         break
 
+
+# this adds likes to posts that contain that search string
 search_string = 'python'
-number_of_tweets = 2
+number_of_tweets = 2  # number of tweets to like
 
 for tweet in tweepy.Cursor(api.search_tweets, search_string).items(number_of_tweets):
     try:
         tweet.favorite()
         print('I liked that tweet')
     except tweepy.TweepyException as e:
-        print(e.reason)
+        print(e)
     except StopIteration:
         break
-
-
