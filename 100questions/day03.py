5c52bd7caa0d971570e4ec08a7dd258cfbe54f30
blu <n1ilmpg@gmail.com>
2023-01-23 14:44:42 +0200

    finishes question 10 again

--- 100questions/day03.py
+++ 100questions/day03.py
@@ -1,4 +1,4 @@
-print("___Question #5:")
+print("___Question #10:")
 
 
 # Write a program that accepts a sequence of whitespace separated words as input and prints the words after removing all duplicate words and sorting them alphanumerically.
