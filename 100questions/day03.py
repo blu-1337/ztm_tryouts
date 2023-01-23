411f0a672c754575d0cc8cbf763a91fa9b1a35e2
blu <n1ilmpg@gmail.com>
2023-01-23 15:21:57 +0200

    finishes question 11

--- 100questions/day03.py
+++ 100questions/day03.py
@@ -36,3 +36,39 @@ g_string = " ".join(g_string)
 
 print(g_string)
 
+print("___Question #11:")
+
+# Write a program which accepts a sequence of comma separated 4 digit binary numbers as its input and then check whether they are divisible by 5 or not. The numbers that are divisible by 5 are to be printed in a comma separated sequence.
+
+# Example:
+
+# 0100,0011,1010,1001
+# Then the output should be:
+
+# 1010
+# Notes: Assume the data is input by console.
+
+# Hints:
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+
+
+def convert_binary_to_decimal(number):
+  i = 0
+  increment = 8
+  sum = 0
+  while(i < 4):
+    if(int(number[i]) == 1):
+      sum = sum + increment 
+    increment /= 2
+    i += 1
+  return int(sum)
+
+
+values = "0100,0011,1010,1001"
+values_list = values.split(',')
+print(values_list)
+
+for number in values_list:
+  if convert_binary_to_decimal(number) % 5 == 0:
+    print(number)
+
