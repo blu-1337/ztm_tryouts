fb02e89ac6858470ebea9da10763e8bf95125d64
blu <n1ilmpg@gmail.com>
2023-01-30 13:04:36 +0200

    finishes quyestion 15 and finishes day 4

--- 100questions/day04.py
+++ 100questions/day04.py
@@ -1,4 +1,17 @@
-print("___Question #12:")
+print("___Question #14:")
+
+# Question:
+# Write a program that accepts a sentence and calculate the number of upper case letters and lower case letters.
+
+# Suppose the following input is supplied to the program:
+
+# Hello world!
+# Then, the output should be:
+
+# UPPER CASE 1
+# LOWER CASE 9
+# Hints:
+# In case of input data being supplied to the question, it should be assumed to be a console input.
 
 
 input_value = "Hello world!"
@@ -18,3 +31,32 @@ for char in input_value:
 
 print("Lower case letters: ", lower_case_letters)
 print("Upper case letters: ", upper_case_letters)
+
+
+print("___Question #15:")
+
+# Question 15
+# Question:
+# Write a program that computes the value of a+aa+aaa+aaaa with a given digit as the value of a.
+
+# Suppose the following input is supplied to the program:
+
+# 9
+# Then, the output should be:
+
+# 11106
+# Hints:
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+
+input_value = 9
+
+sum = 0
+for i in range(1,5):
+  number = ""
+  while i>=1:
+    number += str(input_value)
+    print(number)
+    i -= 1
+  sum += int(number)
+print(sum)
+
