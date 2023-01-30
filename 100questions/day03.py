4e2c7632a44e9d2724c0cb7811ef04c1f037b31b
blu <n1ilmpg@gmail.com>
2023-01-30 12:56:29 +0200

    finishes question 13

--- 100questions/day03.py
+++ 100questions/day03.py
@@ -103,3 +103,34 @@ for number in range(1000,3001):
       break
   if(all_digits_are_even):
     print(number, end=",")
+
+print()
+print("___Question #13:")
+
+# Question:
+# Write a program that accepts a sentence and calculate the number of letters and digits.
+
+# Suppose the following input is supplied to the program:
+
+# hello world! 123
+# Then, the output should be:
+
+# LETTERS 10
+# DIGITS 3
+# Hints:
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+
+import re
+
+input_value = "hello world! 123"
+letters_count = 0
+numbers_count = 0
+
+for char in input_value:
+  if re.search("[a-zA-Z]", char):
+    letters_count += 1
+  if re.search("[0-9]", char):
+    numbers_count += 1
+
+print("numbers_count: ", numbers_count)
+print("letters_count: ", letters_count)
