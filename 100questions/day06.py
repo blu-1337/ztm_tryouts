63e0a83411fbb6a52d733b507481404a3722dbe2
blu <n1ilmpg@gmail.com>
2023-01-31 12:51:41 +0200

    finishes question 19 and day 6

--- 100questions/day06.py
+++ 100questions/day06.py
@@ -33,3 +33,61 @@ password_values = input_values.split(',')
 for value in password_values:
   if re.search('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#@$]).{6,12}$', value):
     print("this is a good password: ", value)
+
+
+
+print("___Question #19:")
+
+# Question:
+# You are required to write a program to sort the (name, age, score) tuples by ascending order where name is string, age and score are numbers. The tuples are input by console. The sort criteria is:
+
+# 1: Sort based on name
+# 2: Then sort based on age
+# 3: Then sort by score
+# The priority is that name > age > score.
+
+# If the following tuples are given as input to the program:
+
+# Tom,19,80
+# John,20,90
+# Jony,17,91
+# Jony,17,93
+# Json,21,85
+# Then, the output of the program should be:
+
+# [('John', '20', '90'), ('Jony', '17', '91'), ('Jony', '17', '93'), ('Json', '21', '85'), ('Tom', '19', '80')]
+# Hints:
+# In case of input data being supplied to the question, it should be assumed to be a console input.We use itemgetter to enable multiple sort keys.
+
+from operator import itemgetter
+
+counter = 0
+tuple_list = []
+current_tuple = []
+while True:
+    input_value = input()
+    if not input_value:
+      tuple_list.append(tuple(current_tuple))
+      break
+    if counter <= 2:
+      current_tuple.append(input_value)    
+      counter += 1
+      continue
+    if counter == 3:
+      tuple_list.append(tuple(current_tuple))
+      current_tuple = []
+      current_tuple.append(input_value)    
+      counter = 1
+      continue
+# print(tuple_list)
+sorted_list = sorted_list = sorted(
+    tuple_list,
+    key=itemgetter(0,1,2),
+)
+
+
+print(sorted_list)
+
+
+
+    
