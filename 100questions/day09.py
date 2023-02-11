7fd78746cc1e5ccab1fdba6196b21c23bdf32859
blu <n1ilmpg@gmail.com>
2023-02-11 11:22:26 +0200

    finishes day 9 and question 30

--- 100questions/day09.py
+++ 100questions/day09.py
@@ -1,45 +1,77 @@
-def fibocalc(number):
-  if(number == 0):
-    return 0
-  if(number == 1):
-    return 1
-  return (fibocalc(number-1)+fibocalc(number-2))
+print("___Question #26:")
+
+# Question:
+# Define a function which can compute the sum of two numbers.
+
+# Hints:
+# Define a function with two numbers as arguments. You can compute the sum in the function and return the value.
+
+def sum_fun(a, b):
+  return a + b;
+
+print(sum_fun(3,5))
+
+print("___Question #27:")
+
+# Question:
+# Define a function that can convert a integer into a string and print it in console.
+
+# Hints:
+# Use str() to convert a number to string.
+
+def int_to_string(integer):
+  return str(integer)
 
 
-nr = 5
-sum = 0
-# count = 1
 
-while(nr > 0):
-  sum += fibocalc(nr)
-  print("aduna: ", fibocalc(nr))
-  # count += 1
-  nr -= 1
+print(int_to_string(23), type(int_to_string(23)))
 
-print("suma: ", sum)
+stringed = lambda n : str(n)
+print(stringed(21), type(stringed(21)))
+
+print("___Question #28:")
+
+# Question:
+# Define a function that can receive two integer numbers in string form and compute their sum and then print it in console.
+
+# Hints:
+# Use int() to convert a string to integer.
+
+two_string = lambda s1, s2: int(s1) + int(s2)
+
+print(two_string(12,23))
+
+print("___Question #29:")
+# Question:
+# Define a function that can accept two strings as input and concatenate them and then print it in console.
+
+# Hints:
+# Use + sign to concatenate the strings.
+
+concatenator = lambda s1, s2: s1+s2
+print(concatenator("jophn","doe"))
+
+print("___Question #30:")
+
+# Question:
+# Define a function that can accept two strings as input and print the string with maximum length in console. If two strings have the same length, then the function should print all strings line by line.
+
+# Hints:
+# Use len() function to get the length of a string.
 
 
-def fibocalc2(number):
-  if(number == 0):
-    return 0
-  if(number == 1):
-    return 1
+somefun = lambda s1, s2: s1 if(len(s1)>=len(s2)) else s2
+print(somefun("titus", "livis"))
 
-  n1 = 0
-  n2 =  1
-  count = 1
 
-  sum = 1
-  while(count < number):
-     n = n1 + n2
-     n1 = n2
-     n2 = n
-     count += 1
-     sum += n
-  
 
-  
-  return sum  
-  
 
-print("new fibo sum sequence for 5: ", fibocalc2(5))
+
+
+
+
+
+
+
+
+
