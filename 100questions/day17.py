6f52d8ce1091dca4925a2dddf431cb944753fedc
blu <n1ilmpg@gmail.com>
2023-02-13 16:00:31 +0200

    finishes day 17 and question 69

--- 100questions/day17.py
+++ 100questions/day17.py
@@ -6,6 +6,13 @@ print("___Question #65:")
 # Hints
 # Use "assert expression" to make assertion.
 
+input_list = [2,4,6,8]
+
+for i in input_list:
+  assert i % 2 == 0
+
+
+
 print("___Question #66:")
 
 # Question
@@ -20,6 +27,10 @@ print("___Question #66:")
 # Hints
 # Use eval() to evaluate an expression.
 
+input_string = "35 + 3"
+
+print(eval(input_string))
+
 print("___Question #67:")
 
 # Question
@@ -28,6 +39,26 @@ print("___Question #67:")
 # Hints
 # Use if/elif to deal with conditions.
 
+input_values = [1,2,5,33,44,222,367,666,777,888,2323,3431,23333,239892, 929889, 999121, 1992111] 
+
+def find_value(numbers, number_to_find):
+	low = 0
+	high = len(numbers) - 1
+
+	while low <= high:
+		middle = low + (high - low) // 2  # Divides and returns the integer value of the quotient. It dumps the digits after the decimal.
+
+		if numbers[middle] == number_to_find:
+			return middle
+		elif numbers[middle] < number_to_find:
+			low = middle + 1
+		else:
+			high = middle - 1
+	return -1
+
+print("the values you would like to find has index of: ", find_value(input_values,777))
+
+
 print("___Question #68:")
 
 # Question
@@ -35,6 +66,14 @@ print("___Question #68:")
 
 # Hints
 # Use random.random() to generate a random float in [0,1].
+
+from random import randrange, random
+def gen_float():
+  return randrange(10,100) + random()
+
+print(gen_float())
+
+
 
 
 
@@ -46,3 +85,9 @@ print("___Question #69:")
 # Hints
 # Use random.random() to generate a random float in [0,1].
 
+
+from random import randrange, random
+def gen_float():
+  return randrange(5,95) + random()
+
+print(gen_float())
